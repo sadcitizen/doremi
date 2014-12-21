d3d498001f0faab8e06e287395044e7bca8b2f78
instanceofpro <instanceofpro@gmail.com>
2014-12-21 16:44:40 +0500

    move to browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,47 @@
 var gulp = require('gulp');
 var pkg = require('./package.json');
-var template = require('gulp-template');
-var preprocess = require('gulp-preprocess');
-var rename = require('gulp-rename');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 var jscs = require('gulp-jscs');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var template = require('gulp-template');
+var mocha = require('gulp-mocha');
+var cfg = {
+    src: './src/*.js',
+    specs: './tests/*.spec.js'
+};
 
-gulp.task('build', function () {
-    return gulp.src(['./src/mint.js'])
-        .pipe(preprocess())
-        .pipe(template({ version: pkg.version }))
-        .pipe(rename({ basename: 'mint' }))
-        .pipe(gulp.dest('build'));
+gulp.task('browserify', function () {
+    return browserify('./src/is.js', { standalone: 'is' })
+        .bundle()
+        .pipe(source('mint.is.js'))
+        .pipe(gulp.dest('./build/'));
 });
 
-gulp.task('jscs', function() {
-    return gulp.src(['./build/mint.js'])
+gulp.task('jscs', function () {
+    return gulp.src(cfg.src)
         .pipe(jscs());
 });
 
 gulp.task('hint', function () {
-    return gulp.src(['./build/mint.js'])
+    return gulp.src(cfg.src)
         .pipe(jshint('./.jshintrc'))
         .pipe(jshint.reporter('default'));
 });
 
-gulp.task('uglify', function () {
-    return gulp.src(['./build/mint.js'])
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(uglify())
-        .pipe(gulp.dest('build'));
+gulp.task('tests', function () {
+    return gulp.src(cfg.specs, { read: false })
+        .pipe(mocha({ reporter: 'spec' }));
 });
 
-gulp.task('default', ['build'/*, 'jscs', 'hint', 'uglify'*/]);
+gulp.task('uglify', function () {
+    return gulp.src('./build/mint.*.js')
+        .pipe(template({ version: pkg.version }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/min'));
+});
+
+gulp.task('default', ['jscs', 'hint', 'browserify', 'uglify']);
--- tests/is.spec.js
+++ tests/is.spec.js
@@ -1,207 +1,222 @@
-/* jshint -W030 */
-/* jshint -W053 */
-describe('_.is.equal()', function () {
+var is = require('../build/min/mint.is.min');
+var expect = require('chai').expect;
+
+describe('is.equal()', function () {
     it('Two absent args should be equal', function () {
-        expect(_.is.equal()).to.equal(true);
+        expect(is.equal()).to.equal(true);
     });
 
     it('One absent arg and undefined should be equal', function () {
-        expect(_.is.equal(undefined)).to.equal(true);
+        expect(is.equal(undefined)).to.equal(true);
     });
 
     it('Undefined should be equal to undefined', function () {
-        expect(_.is.equal(undefined, undefined)).to.equal(true);
+        expect(is.equal(undefined, undefined)).to.equal(true);
     });
 
     it('Null should be equal to null', function () {
-        expect(_.is.equal(null, null)).to.equal(true);
+        expect(is.equal(null, null)).to.equal(true);
     });
 
     it('Undefined should not be equal to null', function () {
-        expect(_.is.equal(undefined, null)).to.equal(false);
+        expect(is.equal(undefined, null)).to.equal(false);
     });
 
     it('True should be equal to true', function () {
-        expect(_.is.equal(true, true)).to.equal(true);
+        expect(is.equal(true, true)).to.equal(true);
     });
 
     it('False should be equal to false', function () {
-        expect(_.is.equal(false, false)).to.equal(true);
+        expect(is.equal(false, false)).to.equal(true);
     });
 
     it('True should not be equal to false', function () {
-        expect(_.is.equal(true, false)).to.equal(false);
+        expect(is.equal(true, false)).to.equal(false);
     });
 
     it('NaN should be equal to NaN', function () {
-        expect(_.is.equal(NaN, NaN)).to.equal(true);
+        expect(is.equal(NaN, NaN)).to.equal(true);
     });
 
     it('Zero should be equal to zero', function () {
-        expect(_.is.equal(0, 0)).to.equal(true);
+        expect(is.equal(0, 0)).to.equal(true);
     });
 
     it('Zero should not be equal to negative zero', function () {
-        expect(_.is.equal(0, -0)).to.equal(false);
+        expect(is.equal(0, -0)).to.equal(false);
     });
 
     it('Infinity should be equal to infinity', function () {
-        expect(_.is.equal(Infinity, Infinity)).to.equal(true);
+        expect(is.equal(Infinity, Infinity)).to.equal(true);
     });
 
     it('Infinity should not be equal to negative infinity', function () {
-        expect(_.is.equal(Infinity, -Infinity)).to.equal(false);
+        expect(is.equal(Infinity, -Infinity)).to.equal(false);
     });
 
     it('42 should be equal to 42', function () {
-        expect(_.is.equal(42, 42)).to.equal(true);
+        expect(is.equal(42, 42)).to.equal(true);
     });
 
     it('42 should not be equal to -42', function () {
-        expect(_.is.equal(42, -42)).to.equal(false);
+        expect(is.equal(42, -42)).to.equal(false);
     });
 
     it('Empty string should be equal to empty string', function () {
-        expect(_.is.equal('', '')).to.equal(true);
+        expect(is.equal('', '')).to.equal(true);
     });
 
     it('String should be equal to yourself', function () {
-        expect(_.is.equal('foo', 'foo')).to.equal(true);
+        expect(is.equal('foo', 'foo')).to.equal(true);
     });
 
     it('String should not be equal to different string', function () {
-        expect(_.is.equal('foo', 'bar')).to.equal(false);
+        expect(is.equal('foo', 'bar')).to.equal(false);
     });
 });
 
-describe('_.is.exists()', function () {
+describe('is.exists()', function () {
     it('Should return false for undefined', function () {
-        expect(_.is.exists()).to.equal(false);
-        expect(_.is.exists(undefined)).to.equal(false);
+        expect(is.exists()).to.equal(false);
+        expect(is.exists(undefined)).to.equal(false);
     });
 
     it('Should return false for null', function () {
-        expect(_.is.exists(null)).to.equal(false);
+        expect(is.exists(null)).to.equal(false);
     });
 
     it('Should return true for other values', function () {
-        expect(_.is.exists(0)).to.equal(true);
-        expect(_.is.exists(42)).to.equal(true);
-        expect(_.is.exists(-42)).to.equal(true);
-        expect(_.is.exists(NaN)).to.equal(true);
-        expect(_.is.exists(Infinity)).to.equal(true);
-        expect(_.is.exists(-Infinity)).to.equal(true);
-        expect(_.is.exists(true)).to.equal(true);
-        expect(_.is.exists(false)).to.equal(true);
-        expect(_.is.exists('')).to.equal(true);
-        expect(_.is.exists('hello')).to.equal(true);
-        expect(_.is.exists([])).to.equal(true);
-        expect(_.is.exists([1, 2, 3])).to.equal(true);
-        expect(_.is.exists({})).to.equal(true);
-        expect(_.is.exists({a: 'b', c: 'd'})).to.equal(true);
+        expect(is.exists(0)).to.equal(true);
+        expect(is.exists(42)).to.equal(true);
+        expect(is.exists(-42)).to.equal(true);
+        expect(is.exists(NaN)).to.equal(true);
+        expect(is.exists(Infinity)).to.equal(true);
+        expect(is.exists(-Infinity)).to.equal(true);
+        expect(is.exists(true)).to.equal(true);
+        expect(is.exists(false)).to.equal(true);
+        expect(is.exists('')).to.equal(true);
+        expect(is.exists('hello')).to.equal(true);
+        expect(is.exists([])).to.equal(true);
+        expect(is.exists([1, 2, 3])).to.equal(true);
+        expect(is.exists({})).to.equal(true);
+        expect(is.exists({a: 'b', c: 'd'})).to.equal(true);
     });
 });
 
-describe('_.is.float()', function () {
+describe('is.float()', function () {
     it('Should return true for float', function () {
-        expect(_.is.float(42.42)).to.equal(true);
+        expect(is.float(42.42)).to.equal(true);
     });
 
     it('Should return false for other numbers', function () {
-        expect(_.is.float(42)).to.equal(false);
-        expect(_.is.float(0)).to.equal(false);
+        expect(is.float(42)).to.equal(false);
+        expect(is.float(0)).to.equal(false);
     });
 });
 
-describe('_.is.int()', function () {
+describe('is.int()', function () {
     it('Should return true for integer', function () {
-        expect(_.is.int(42)).to.equal(true);
+        expect(is.int(42)).to.equal(true);
     });
 
     it('Should return true for zero', function () {
-        expect(_.is.int(0)).to.equal(true);
+        expect(is.int(0)).to.equal(true);
     });
 
     it('Should return false for float', function () {
-        expect(_.is.int(42.42)).to.equal(false);
+        expect(is.int(42.42)).to.equal(false);
     });
 });
 
-describe('_.is.primitive()', function () {
+describe('is.primitive()', function () {
     it('Boolean should be primitive', function () {
-        expect(_.is.primitive(true)).to.equal(true);
-        expect(_.is.primitive(false)).to.equal(true);
-        expect(_.is.primitive(new Boolean(false))).to.equal(true);
-        expect(_.is.primitive(Boolean(true))).to.equal(true);
+        expect(is.primitive(true)).to.equal(true);
+        expect(is.primitive(false)).to.equal(true);
+        /* jshint -W053 */
+        expect(is.primitive(new Boolean(false))).to.equal(true);
+        /* jshint +W053 */
+        expect(is.primitive(Boolean(true))).to.equal(true);
     });
 
     it('String should be primitive', function () {
-        expect(_.is.primitive('foo')).to.equal(true);
-        expect(_.is.primitive(new String('foo'))).to.equal(true);
-        expect(_.is.primitive(String('foo'))).to.equal(true);
-        expect(_.is.primitive('')).to.equal(true);
+        expect(is.primitive('foo')).to.equal(true);
+        /* jshint -W053 */
+        expect(is.primitive(new String('foo'))).to.equal(true);
+        /* jshint +W053 */
+        expect(is.primitive(String('foo'))).to.equal(true);
+        expect(is.primitive('')).to.equal(true);
     });
 
     it('Number should be primitive', function () {
-        expect(_.is.primitive(42)).to.equal(true);
-        expect(_.is.primitive(new Number(42))).to.equal(true);
-        expect(_.is.primitive(Number(42))).to.equal(true);
-        expect(_.is.primitive(0)).to.equal(true);
-        expect(_.is.primitive(-0)).to.equal(true);
+        expect(is.primitive(42)).to.equal(true);
+        /* jshint -W053 */
+        expect(is.primitive(new Number(42))).to.equal(true);
+        /* jshint +W053 */
+        expect(is.primitive(Number(42))).to.equal(true);
+        expect(is.primitive(0)).to.equal(true);
+        expect(is.primitive(-0)).to.equal(true);
     });
 
     it('Null should be primitive', function () {
-        expect(_.is.primitive(null)).to.equal(true);
+        expect(is.primitive(null)).to.equal(true);
     });
 
     it('Undefined should be primitive', function () {
-        expect(_.is.primitive(undefined)).to.equal(true);
-        expect(_.is.primitive()).to.equal(true);
+        expect(is.primitive(undefined)).to.equal(true);
+        expect(is.primitive()).to.equal(true);
     });
 });
 
-describe('_.is.type()', function () {
+describe('is.type()', function () {
     it('Should return a type of value', function () {
-        expect(_.is.type(arguments)).to.equal('arguments');
+        expect(is.type(arguments)).to.equal('arguments');
 
-        expect(_.is.type([])).to.equal('array');
-        expect(_.is.type([1, 2, 3])).to.equal('array');
-        expect(_.is.type(new Array(1, 2, 3))).to.equal('array');
+        expect(is.type([])).to.equal('array');
+        expect(is.type([1, 2, 3])).to.equal('array');
+        expect(is.type(new Array(1, 2, 3))).to.equal('array');
 
-        expect(_.is.type(true)).to.equal('boolean');
-        expect(_.is.type(false)).to.equal('boolean');
-        expect(_.is.type(new Boolean(false))).to.equal('boolean');
-        expect(_.is.type(Boolean(true))).to.equal('boolean');
+        expect(is.type(true)).to.equal('boolean');
+        expect(is.type(false)).to.equal('boolean');
+        /* jshint -W053 */
+        expect(is.type(new Boolean(false))).to.equal('boolean');
+        /* jshint +W053 */
+        expect(is.type(Boolean(true))).to.equal('boolean');
 
-        expect(_.is.type(new Date())).to.equal('date');
+        expect(is.type(new Date())).to.equal('date');
 
-        expect(_.is.type(function () {
+        expect(is.type(function () {
         })).to.equal('function');
-        expect(_.is.type(new Function('return true'))).to.equal('function');
-        expect(_.is.type(Math.abs)).to.equal('function');
+        /* jshint -W054 */
+        expect(is.type(new Function('return true'))).to.equal('function');
+        /* jshint +W054 */
+        expect(is.type(Math.abs)).to.equal('function');
 
-        expect(_.is.type(undefined)).to.equal('undefined');
+        expect(is.type(undefined)).to.equal('undefined');
 
-        expect(_.is.type(NaN)).to.equal('nan');
+        expect(is.type(NaN)).to.equal('nan');
 
-        expect(_.is.type(42)).to.equal('number');
-        expect(_.is.type(new Number(42))).to.equal('number');
-        expect(_.is.type(Number(42))).to.equal('number');
-        expect(_.is.type(0)).to.equal('number');
-        expect(_.is.type(-0)).to.equal('number');
+        expect(is.type(42)).to.equal('number');
+        /* jshint -W053 */
+        expect(is.type(new Number(42))).to.equal('number');
+        /* jshint +W053 */
+        expect(is.type(Number(42))).to.equal('number');
+        expect(is.type(0)).to.equal('number');
+        expect(is.type(-0)).to.equal('number');
 
-        expect(_.is.type(Infinity)).to.equal('infinity');
+        expect(is.type(Infinity)).to.equal('infinity');
 
-        expect(_.is.type(null)).to.equal('null');
+        expect(is.type(null)).to.equal('null');
 
-        expect(_.is.type({})).to.equal('object');
+        expect(is.type({})).to.equal('object');
 
-        expect(_.is.type(/\s+/ig)).to.equal('regexp');
-        expect(_.is.type(new RegExp('\\s+', 'ig'))).to.equal('regexp');
+        expect(is.type(/\s+/ig)).to.equal('regexp');
+        expect(is.type(new RegExp('\\s+', 'ig'))).to.equal('regexp');
 
-        expect(_.is.type('foo')).to.equal('string');
-        expect(_.is.type(new String('foo'))).to.equal('string');
-        expect(_.is.type(String('foo'))).to.equal('string');
-        expect(_.is.type('')).to.equal('string');
+        expect(is.type('foo')).to.equal('string');
+        /* jshint -W053 */
+        expect(is.type(new String('foo'))).to.equal('string');
+        /* jshint +W053 */
+        expect(is.type(String('foo'))).to.equal('string');
+        expect(is.type('')).to.equal('string');
     });
 });
