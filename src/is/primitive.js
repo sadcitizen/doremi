de6cc6d4f9dcbfdd9e080a53d8d7d5aaed595f90
instanceofpro <instanceofpro@gmail.com>
2014-12-27 13:44:11 +0500

    add description for is.primitive

--- src/is/primitive.js
+++ src/is/primitive.js
@@ -1,6 +1,22 @@
 var type = require('./type');
 var primitives = ['boolean', 'number', 'string', 'undefined', 'null'];
 
+/**
+ * Check if `target` is a primitive value.
+ * Primitive values are booleans, numbers, strings, `undefined` and `null`.
+ * Equivalent object wrappers for primitive values are also primitive.
+ *
+ * @param target The value to check.
+ * @returns {boolean} Returns `true` if `target` is a primitive, else `false`.
+ *
+ * @example
+ *
+ * is.primitive(42);
+ * // => true
+ *
+ * is.primitive([]);
+ * // => false
+ */
 module.exports = function primitive(target) {
     return primitives.indexOf(type(target)) !== -1;
 };
