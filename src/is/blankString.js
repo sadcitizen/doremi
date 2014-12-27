4e0d48277d05116aea38471e509ad4c09c705622
instanceofpro <instanceofpro@gmail.com>
2014-12-27 14:54:02 +0500

    fix is.blankString

--- src/is/blankString.js
+++ src/is/blankString.js
@@ -1,5 +1,5 @@
 var WHITESPACES_REGEX = /^[\s\0\b\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]*$/;
-
+var string = require('./string');
 /**
  * Checks if `target` is a blank string.
  *
@@ -18,5 +18,5 @@ var WHITESPACES_REGEX = /^[\s\0\b\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\
  * // => true
  */
 module.exports = function blankString(target) {
-    return WHITESPACES_REGEX.test(target);
+    return string(target) && WHITESPACES_REGEX.test(target);
 };
