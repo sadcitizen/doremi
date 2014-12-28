482a4d845d0f01a162f04a64bde95b0016768029
instanceofpro <instanceofpro@gmail.com>
2014-12-28 22:30:33 +0500

    add tests to object.result

--- tests/object.spec.js
+++ tests/object.spec.js
@@ -40,3 +40,38 @@ describe('object.get()', function () {
         expect(object.get(foo, 'foo.foo.foo.foo.foo.foo')).to.equal(undefined);
     });
 });
+
+describe('object.result()', function () {
+    var obj;
+
+    before(function () {
+        obj = {
+            attr: 'value',
+            arr: [0, 1, 2],
+            num: 2,
+            falsey: '',
+            method: function () {
+                return this.attr;
+            }
+        };
+    });
+
+    it('Should return nothing for undefined object properties.', function() {
+        expect(object.result(obj, 'some')).to.equal(undefined);
+    });
+
+    it('Should return default value if property on an object is undefined', function() {
+        expect(object.result(obj, 'some', true)).to.equal(true);
+        expect(object.result(obj, 'other', 42)).to.equal(42);
+    });
+
+    it('Should evaluate a method with object context and return its result.', function() {
+        expect(object.result(obj, 'method')).to.equal('value');
+    });
+
+    it('Should evaluate an attribute and return its result.', function() {
+        expect(object.result(obj, 'attr')).to.equal('value');
+        expect(object.result(obj, 'falsey')).to.equal('');
+        expect(object.result(obj, 'arr')).to.deep.equal([0, 1, 2]);
+    });
+});
