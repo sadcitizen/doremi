a0cd8ed0760ecbd3156ea09eb7e579959ed839fe
instanceofpro <instanceofpro@gmail.com>
2015-01-04 00:53:38 +0500

    add description for fn.compose and fn.pipeline

--- src/fn/compose.js
+++ src/fn/compose.js
@@ -1,6 +1,26 @@
 var pipeline = require('./pipeline');
 var reverse = require('../internal/reverse');
 
+/**
+ * Create a new function that will compose and invoke functions from right-to-left,
+ * passing the return values from each invocation to the next successive function.
+ *
+ * @returns {Function} Returns new function.
+ *
+ * @example
+ *
+ * function sum(x, y) {
+ *      return x + y;
+ * }
+ *
+ * function square(x) {
+ *      return x * x;
+ * }
+ *
+ * var squareOfSum = fn.compose(square, sum);
+ * squareOfSum(2, 3);
+ * // => 25
+ */
 module.exports = function compose() {
     return pipeline.apply(null, reverse(arguments));
 };
--- src/fn/pipeline.js
+++ src/fn/pipeline.js
@@ -1,6 +1,26 @@
 var slice = require('../internal/slice');
 var noop = require('./noop');
 
+/**
+ * Create a new function that will compose and invoke functions from left-to-right,
+ * passing the return values from each invocation to the next successive function.
+ *
+ * @returns {Function} Returns new function.
+ *
+ * @example
+ *
+ * function sum(x, y) {
+ *      return x + y;
+ * }
+ *
+ * function square(x) {
+ *      return x * x;
+ * }
+ *
+ * var squareOfSum = fn.pipeline(sum, square);
+ * squareOfSum(2, 3);
+ * // => 25
+ */
 module.exports = function pipeline() {
     var fns = slice(arguments);
 
