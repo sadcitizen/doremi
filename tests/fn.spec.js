09c5c521bd1aac608400d7968d5af7e90bae9984
instanceofpro <instanceofpro@gmail.com>
2015-01-08 21:01:45 +0500

    ref

--- tests/fn.spec.js
+++ tests/fn.spec.js
@@ -2,74 +2,76 @@ var fn = require('../src/fn');
 var expect = require('chai').expect;
 var sinon = require('sinon');
 
-describe('fn.compose()', function () {
-    var fn1 = sinon.spy();
-    var fn2 = sinon.spy();
-    var fn3 = sinon.spy();
-    var composed = fn.compose(fn1, fn2, fn3);
+describe('fn', function () {
+    describe('fn.compose()', function () {
+        var fn1 = sinon.spy();
+        var fn2 = sinon.spy();
+        var fn3 = sinon.spy();
+        var composed = fn.compose(fn1, fn2, fn3);
 
-    it('Should return new function', function () {
-        expect(composed).to.be.a('function');
+        it('Should return new function', function () {
+            expect(composed).to.be.a('function');
+        });
+
+        it('Should call all provided functions', function () {
+            composed();
+            expect(fn1.called).to.equal(true);
+            expect(fn2.called).to.equal(true);
+            expect(fn3.called).to.equal(true);
+        });
+
+        it('Should return result of invoking the provided functions', function () {
+            function f1 (n) {
+                return n + 1;
+            }
+
+            function f2 (n) {
+                return n * 2;
+            }
+
+            function f3 (n) {
+                return n - 3;
+            }
+
+            expect(fn.compose(f1)(1)).to.equal(2);
+            expect(fn.compose(f2, f1)(1)).to.equal(4);
+            expect(fn.compose(f3, f2, f1)(1)).to.equal(1);
+        });
     });
 
-    it('Should call all provided functions', function () {
-        composed();
-        expect(fn1.called).to.equal(true);
-        expect(fn2.called).to.equal(true);
-        expect(fn3.called).to.equal(true);
-    });
+    describe('fn.pipeline()', function () {
+        var fn1 = sinon.spy();
+        var fn2 = sinon.spy();
+        var fn3 = sinon.spy();
+        var pipelined = fn.pipeline(fn1, fn2, fn3);
 
-    it('Should return result of invoking the provided functions', function () {
-        function f1 (n) {
-            return n + 1;
-        }
+        it('Should return new function', function () {
+            expect(pipelined).to.be.a('function');
+        });
 
-        function f2 (n) {
-            return n * 2;
-        }
+        it('Should call all provided functions', function () {
+            pipelined();
+            expect(fn1.called).to.equal(true);
+            expect(fn2.called).to.equal(true);
+            expect(fn3.called).to.equal(true);
+        });
 
-        function f3 (n) {
-            return n - 3;
-        }
+        it('Should return result of invoking the provided functions', function () {
+            function f1 (n) {
+                return n + 1;
+            }
 
-        expect(fn.compose(f1)(1)).to.equal(2);
-        expect(fn.compose(f2, f1)(1)).to.equal(4);
-        expect(fn.compose(f3, f2, f1)(1)).to.equal(1);
-    });
-});
+            function f2 (n) {
+                return n * 2;
+            }
 
-describe('fn.pipeline()', function () {
-    var fn1 = sinon.spy();
-    var fn2 = sinon.spy();
-    var fn3 = sinon.spy();
-    var pipelined = fn.pipeline(fn1, fn2, fn3);
+            function f3 (n) {
+                return n - 3;
+            }
 
-    it('Should return new function', function () {
-        expect(pipelined).to.be.a('function');
-    });
-
-    it('Should call all provided functions', function () {
-        pipelined();
-        expect(fn1.called).to.equal(true);
-        expect(fn2.called).to.equal(true);
-        expect(fn3.called).to.equal(true);
-    });
-
-    it('Should return result of invoking the provided functions', function () {
-        function f1 (n) {
-            return n + 1;
-        }
-
-        function f2 (n) {
-            return n * 2;
-        }
-
-        function f3 (n) {
-            return n - 3;
-        }
-
-        expect(fn.pipeline(f1)(1)).to.equal(2);
-        expect(fn.pipeline(f1, f2)(1)).to.equal(4);
-        expect(fn.pipeline(f1, f2, f3)(1)).to.equal(1);
+            expect(fn.pipeline(f1)(1)).to.equal(2);
+            expect(fn.pipeline(f1, f2)(1)).to.equal(4);
+            expect(fn.pipeline(f1, f2, f3)(1)).to.equal(1);
+        });
     });
 });
