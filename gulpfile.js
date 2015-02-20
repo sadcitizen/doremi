9077b5553016948ceb62248a60367f566f6bd568
instanceofpro <instanceofpro@gmail.com>
2015-02-20 19:50:51 +0500

    remove unnecessary

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-var pkg = require('./package.json');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
@@ -7,7 +6,6 @@ var jscs = require('gulp-jscs');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-var template = require('gulp-template');
 var mocha = require('gulp-mocha');
 var buffer = require('vinyl-buffer');
 var cfg = {
@@ -48,14 +46,6 @@ gulp.task('hint', function () {
 gulp.task('tests', function () {
     return gulp.src(cfg.specs, { read: false })
         .pipe(mocha({ reporter: 'spec' }));
-});
-
-gulp.task('uglify', function () {
-    return gulp.src('./build/*.js')
-        .pipe(template({ version: pkg.version }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(uglify())
-        .pipe(gulp.dest('build/min'));
 });
 
 gulp.task('default', ['jscs', 'hint', 'browserify']);
