94be340c6e9a47d4a9e5bf5d8ecb391895c560b8
instanceofpro <instanceofpro@gmail.com>
2015-03-29 00:12:53 +0500

    add array.compact

--- src/array.js
+++ src/array.js
@@ -1,7 +1,15 @@
+import at from './array/at';
+import compact from './array/compact';
 import each from './array/each';
+import first from './array/first';
+import last from './array/last';
 import rest from './array/rest';
 
 export default {
+    at: at,
+    compact: compact,
     each: each,
+    first: first,
+    last: last,
     rest: rest
 };
---
+++ src/array/compact.js
@@ -0,0 +1,5 @@
+function compact(target) {
+    return target.filter(Boolean);
+}
+
+export default compact;
