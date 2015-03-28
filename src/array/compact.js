e8325c517f9c3d80556f3d645b45fa14f1b9b077
instanceofpro <instanceofpro@gmail.com>
2015-03-29 00:19:29 +0500

    add description for array.compact

--- src/array/compact.js
+++ src/array/compact.js
@@ -1,3 +1,18 @@
+/**
+ * Returns a copy of the `target` with all falsey values removed.
+ *
+ * @param target The array to compact.
+ * @returns {Array}
+ *
+ * @example
+ *
+ * compact([1, 2, 3]);
+ * // => [1, 2, 3]
+ *
+ * compact([null, 1, false]);
+ * // => [1]
+ */
+
 function compact(target) {
     return target.filter(Boolean);
 }
