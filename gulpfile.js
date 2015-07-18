a20022a46d7a24f09eb4cdb3a2a621435501db32
instanceofpro <instanceofpro@gmail.com>
2015-07-18 16:45:38 +0500

    run tests with babel

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,10 @@ var cfg = {
     src: './src/*.js',
     dest: './build/',
     tmp: './tmp/',
-    specs: './tests/*.spec.js'
+    specs: './tests/**/*.spec.js'
 };
+
+require('babel/register');
 
 gulp.task('browserify', function () {
     var bundlify = function (src) {
@@ -45,7 +47,10 @@ gulp.task('hint', function () {
 
 gulp.task('tests', function () {
     return gulp.src(cfg.specs, { read: false })
-        .pipe(mocha({ reporter: 'spec' }));
+        .pipe(mocha({
+            reporter: 'spec',
+            compilers: 'babel'
+        }));
 });
 
 gulp.task('default', ['jscs', 'hint', 'browserify']);
