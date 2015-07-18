d6270947b4eba06ae30104536c861d1c1c24d6fb
instanceofpro <instanceofpro@gmail.com>
2015-07-18 20:27:59 +0500

    add description to random.bool

--- src/random/bool.js
+++ src/random/bool.js
@@ -1,4 +1,15 @@
-// TODO: add description
+/**
+ * Generates a pseudo-random boolean value.
+ * @returns {boolean} Returns the boolean value.
+ *
+ * @example
+ *
+ * bool();
+ * // => true
+ *
+ * bool();
+ * // => false
+ */
 function bool() {
     return Math.random() < 0.5;
 }
