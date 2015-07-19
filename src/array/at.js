5713ceb99746453bcb14a843f58719a03ca86de0
instanceofpro <instanceofpro@gmail.com>
2015-07-19 17:00:01 +0500

    add description to array.at

--- src/array/at.js
+++ src/array/at.js
@@ -1,6 +1,23 @@
 import isArray from '../is/isArray';
 
-// TODO: add description
+/**
+ * Returns an array of elements corresponding to the given indexes.
+ *
+ * @param target {Array} The array to iterate over.
+ * @param indexes {Array} The indexes of elements to pick.
+ * @returns {*|Array} The array of elements.
+ *
+ * @example
+ *
+ * at([1, 2, 3, 4, 5], 1);
+ * // => 2
+ *
+ * at([1, 2, 3, 4, 5], 1, 3);
+ * // => [2, 4]
+ *
+ * at([1, 2, 3, 4, 5], [-1, -2, 0]);
+ * // => [5, 4, 1]
+ */
 
 function at(target, ...indexes) {
     let index;
