aadaf51c5346e1ffb67577e573b8de1c59400ff1
instanceofpro <instanceofpro@gmail.com>
2015-07-19 16:55:11 +0500

    add description to array.last and array.first

--- src/array/last.js
+++ src/array/last.js
@@ -1,6 +1,24 @@
 import slice from '../internal/slice';
 
-// TODO: add description
+/**
+ * Returns the last element of an array. Passing `count`
+ * will return the last `count` elements of the array.
+ *
+ * @param target {Array} The array of elements.
+ * @param count {number} The required count of elements.
+ * @returns {*|Array} Returns elements of the array.
+ *
+ * @example
+ *
+ * last([1, 2, 3, 4, 5]);
+ * // => 5
+ *
+ * last([1, 2, 3, 4, 5], 3);
+ * // => [3, 4, 5]
+ *
+ * last([1, 2, 3, 4, 5], 99);
+ * // => [1, 2, 3, 4, 5]
+ */
 
 function last(target, count = 1) {
     let length = target.length;
