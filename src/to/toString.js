2bb075917695ddd374dd2afaab406a8d867a4352
instanceofpro <instanceofpro@gmail.com>
2015-07-19 16:18:52 +0500

    add toString

--- src/to/toString.js
+++ src/to/toString.js
@@ -1,8 +1,24 @@
 import isExists from '../is/isExists';
 
 /**
+ * Returns a string representation of `target`.
  *
- * @param {*} target
- * @returns {String}
+ * @param {*} target The value to convert.
+ * @returns {String} The string representation of `target`.
+ *
+ * @example
+ *
+ * toString(123);
+ * // => '123'
+ *
+ * toString(null);
+ * // => ''
+ *
+ * toString(undefined);
+ * // => ''
+ *
+ * toString(/[a-z0-9]/igm);
+ * // => '/[a-z0-9]/gim'
  */
+//TODO: Array, Object, Date, Function
 export default target => !isExists(target) ? '' : target.toString();
