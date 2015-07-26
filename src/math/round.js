b072a3bbc4221f63c0fb10cc6273f7bb9f0041e0
instanceofpro <instanceofpro@gmail.com>
2015-07-26 18:03:58 +0500

    add description to math.round, math.ceil and math.floor

--- src/math/round.js
+++ src/math/round.js
@@ -2,4 +2,19 @@
 
 import baseRound from '../internal/baseRound';
 
+/**
+ * Creates number rounded to precision.
+ *
+ * @param {number} num The number to round.
+ * @param {number} precision The precision to round to.
+ * @returns {number} Returns the rounded number.
+ *
+ * @example
+ *
+ * round(42.52);
+ * // => 43
+ *
+ * round(5.4321, 2);
+ * // => 5.43
+ */
 export default baseRound('round');
