addb5bcb43ed0e94a889d72dc98c27da2dc9d490
instanceofpro <instanceofpro@gmail.com>
2015-07-30 08:22:29 +0500

    add description to number.fixed

--- src/number/fixed.js
+++ src/number/fixed.js
@@ -3,9 +3,17 @@
 /**
  * Formats a number using fixed-point notation (like Number.toFixed(), but without rounding).
  *
- * @param target
- * @param precision
- * @returns {string}
+ * @param {number} target The number to format.
+ * @param {number} precision The precision to format to.
+ * @returns {string} Returns the formatted number.
+ *
+ * @example
+ *
+ * fixed(12.345);
+ * // => '12.34'
+ *
+ * (12.345).toFixed(2);
+ * // => '12.35'
  */
 
 export default function (target, precision = 2) {
