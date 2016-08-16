a4c24fd6a25da36e9f2e21ab1231f46e47c15b9d
sadcitizen <sadcitizen@yandex.ru>
2016-08-17 00:16:12 +0500

    function declaration -> arrow function

--- src/random/guid.js
+++ src/random/guid.js
@@ -13,8 +13,5 @@ import choice from './choice';
  * guid();
  * // => 84d82ea7-a8d8-4b94-add5-818650da2ea5
  */
-function guid() {
-    return `${hex(8)}-${hex(4)}-4${hex(3)}-${choice(8, 9, 'a', 'b')}${hex(3)}-${hex(12)}`;
-}
 
-export default guid;
+export default () => `${hex(8)}-${hex(4)}-4${hex(3)}-${choice(8, 9, 'a', 'b')}${hex(3)}-${hex(12)}`;
