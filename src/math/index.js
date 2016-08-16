f11c9976d5474f0146afe9602a8476b1306d6fad
sadcitizen <sadcitizen@yandex.ru>
2016-08-16 23:41:34 +0500

    index for math and random

---
+++ src/math/index.js
@@ -0,0 +1,11 @@
+import ceil from './ceil';
+import floor from './floor';
+import log from './log';
+import round from './round';
+
+export default {
+    ceil,
+    floor,
+    log,
+    round
+};
---
+++ src/random/index.js
@@ -0,0 +1,15 @@
+import bool from './bool';
+import choice from './choice';
+import float from './float';
+import guid from './guid';
+import hex from './hex';
+import integer from './integer';
+
+export default {
+    bool,
+    choice,
+    float,
+    guid,
+    hex,
+    integer
+};
