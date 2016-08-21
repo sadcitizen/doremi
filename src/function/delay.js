77d492479871422ea4ba340e9c5e0f9b1ae56c1f
sadcitizen <sadcitizen@yandex.ru>
2016-08-22 01:41:25 +0500

    add description for function/delay

--- src/function/delay.js
+++ src/function/delay.js
@@ -1,9 +1,21 @@
-// TODO: add description
+/**
+ * Returns a new function that will always invoke `target` after `ms` milliseconds.
+ *
+ * @param {Function} target The function to invoke after a delay.
+ * @param {number} ms The number of milliseconds to wait before invoking `target`.
+ * @param {*} context
+ * @returns {Function} Returns the new delayed function.
+ *
+ * @example
+ *
+ * const delayedLog = delay(console.log, 50);
+ * delayedLog('Hi!');
+ * => 'Hi!'
+ */
 
-function delay(target, ms) {
-    return function () {
-        var args = arguments;
-        setTimeout(() => target.apply(null, args), ms);
+function delay(target, ms, context = null) {
+    return function (...args) {
+        setTimeout(() => target.apply(context, args), ms);
     };
 }
 
