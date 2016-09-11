abf0688862a404ae7d6dcec52318912a6d8133f8
Edele Gizatullin <me@edele.ru>
2016-09-11 12:46:14 +0500

    date/diff args check

--- src/date/diff.js
+++ src/date/diff.js
@@ -3,8 +3,12 @@
 import units from '../internal/time-units';
 
 function diff(first, second, unit) {
+    if (first === undefined || second === undefined)
+        throw new Error('diff() expects at least two arguments');
+
     const diffInMs = second - first;
-    return diffInMs / units.get(unit);
+    const diff = diffInMs / units.get(unit);
+    return Math.round(diff);
 }
 
 export default diff;
