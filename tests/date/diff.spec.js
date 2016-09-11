cf564cd1096206fe7dcb72dc0811c31fb0762dbd
Edele Gizatullin <me@edele.ru>
2016-09-11 12:46:29 +0500

    date/diff tests

--- tests/date/diff.spec.js
+++ tests/date/diff.spec.js
@@ -3,24 +3,93 @@
 import { expect } from 'chai';
 import diff from '../../src/date/diff';
 
-const createDate = (year, month, day) => new Date(Date.UTC(year, month, day));
-
 describe('date/diff()', () => {
-    it('in days', () => {
-        const from = createDate(2000, 0, 1);
-        const oneDayMore = createDate(2000, 0, 2);
-        const nextMonth = createDate(2000, 1, 1);
+    it('in years', () => {
+        const from = new Date(2000, 0);
+        const twoYearsAgo = new Date(2002, 0);
 
-        expect(diff(from, from, 'day')).to.be.equal(0);
-        expect(diff(from, oneDayMore, 'days')).to.be.equal(1);
-        expect(diff(from, nextMonth, 'day')).to.be.equal(31);
+        expect(diff(from, from, 'years')).to.be.equal(0);
+        expect(diff(from, twoYearsAgo, 'y')).to.be.equal(2);
+    });
+
+    it('in months', () => {
+        const from = new Date(2000, 0);
+        const nextYear = new Date(2001, 0);
+
+        expect(diff(from, from, 'months')).to.be.equal(0);
+        expect(diff(from, nextYear, 'M')).to.be.equal(12);
     });
 
     it('in weeks', () => {
-        const from = createDate(2000, 0, 1);
-        const nextWeek = createDate(2000, 0, 8);
+        const from = new Date(2000, 0, 1);
+        const nextWeek = new Date(2000, 0, 8);
 
         expect(diff(from, from, 'weeks')).to.be.equal(0);
-        expect(diff(from, nextWeek, 'weeks')).to.be.equal(1);
+        expect(diff(from, nextWeek, 'w')).to.be.equal(1);
+    });
+
+    it('in days', () => {
+        const from = new Date(2000, 0, 1);
+        const oneDayMore = new Date(2000, 0, 2);
+        const nextMonth = new Date(2000, 1, 1);
+
+        expect(diff(from, from, 'days')).to.be.equal(0);
+        expect(diff(from, oneDayMore, 'd')).to.be.equal(1);
+        expect(diff(from, nextMonth, 'days')).to.be.equal(31);
+    });
+
+    it('in seconds', () => {
+        const from = new Date(2000, 0, 1, 0, 0, 0, 0);
+        const nextSecond = new Date(2000, 0, 1, 0, 0, 1, 0);
+
+        expect(diff(from, nextSecond, 'seconds')).to.be.equal(1);
+        expect(diff(from, nextSecond, 's')).to.be.equal(1);
+    });
+
+    it('in milliseconds', () => {
+        const from = new Date(2000, 0, 1, 0, 0, 0, 0);
+        const nextSecond = new Date(2000, 0, 1, 0, 0, 1, 0);
+
+        expect(diff(from, nextSecond, 'milliseconds')).to.be.equal(1000);
+        expect(diff(from, nextSecond, 'ms')).to.be.equal(1000);
+    });
+
+    it('defaults to milliseconds when unit is undefined', () => {
+        const from = new Date(2000, 0, 1, 0, 0, 0, 0);
+        const nextSecond = new Date(2000, 0, 1, 0, 0, 1, 0);
+
+        expect(diff(from, nextSecond)).to.be.equal(1000);
+    });
+
+    it('results are rounded', () => {
+        const from = new Date(2000, 0, 1);
+        const dayAndSomeMore = new Date(2000, 0, 2, 11);
+        const almostTwoDaysAfter = new Date(2000, 0, 2, 21);
+        const yearAndSomeMore = new Date(2001, 4, 16);
+
+        expect(diff(from, dayAndSomeMore, 'd')).to.be.equal(1);
+        expect(diff(from, almostTwoDaysAfter, 'd')).to.be.equal(2);
+        expect(diff(from, dayAndSomeMore, 'y')).to.be.equal(0);
+        expect(diff(from, yearAndSomeMore, 'y')).to.be.equal(1);
+    });
+
+    it('negative diff result', () => {
+        const from = new Date(2000, 0, 1);
+        const oneDayMore = new Date(2000, 0, 2);
+        const nextMonth = new Date(2000, 1, 1);
+
+        expect(diff(oneDayMore, from, 'd')).to.be.equal(-1);
+        expect(diff(nextMonth, from, 'd')).to.be.equal(-31);
+    });
+
+    it.skip('throws on unknown unit', () => {
+        const d = new Date()
+        expect(diff(d, d, 'cheese!&')).to.throw();
+    });
+
+    it.skip('throws on undefined args', () => {
+        const d = new Date()
+        expect(diff(d)).to.throw();
+        expect(diff()).to.throw();
     });
 });
