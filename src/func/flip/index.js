501cd23378a74379813760d0d6927d167e61a5b8
abrosimov <abrosimov@byndyusoft.com>
2016-10-27 15:59:06 +0500

    add description for func/flip

--- src/func/flip/index.js
+++ src/func/flip/index.js
@@ -1,6 +1,15 @@
 /**
- * [description]
- * @param  {Function} fn [description]
- * @return {[type]}      [description]
+ * Return a new function which accepts arguments in the reverse order that the original function specifies.
+ * @param  {Function} fn Thefunction which needs to accept arguments in reverse order.
+ * @return {Function} The new function.
+ *
+ * @example
+ * const divide = (x, y) => x / y;
+ * const flippedDivide = flip(divide);
+ *
+ * divide(100, 10);
+ * // => 10
+ * flippedDivide(100, 10);
+ * // => 0.1
  */
 export default fn => (...args) => fn(...args.reverse());
