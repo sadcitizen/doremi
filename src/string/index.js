aa9e7204ee3d13d62719889d8e48120eff6bb10a
sadcitizen <sadcitizen@yandex.ru>
2016-11-05 16:07:19 +0500

    add index for string functions

---
+++ src/string/index.js
@@ -0,0 +1,88 @@
+import camelize from './camelize';
+import capitalize from './capitalize';
+import chop from './chop';
+import chopRight from './chop-right';
+import clean from './clean';
+import count from './count';
+import decapitalize from './decapitalize';
+import escape from './escape';
+import extract from './extract';
+import insert from './insert';
+import isBlank from './is-blank';
+import isChar from './is-char';
+import isString from './is-string';
+import join from './join';
+import lines from './lines';
+import pad from './pad';
+import padLeft from './pad-left';
+import padRight from './pad-right';
+import remove from './remove';
+import repeat from './repeat';
+import reverse from './reverse';
+import swap from './swap';
+import template from './template';
+import trim from './trim';
+import trimLeft from './trim-left';
+import trimRight from './trim-right';
+import truncate from './truncate';
+import unescape from './unescape';
+
+export { camelize };
+export { capitalize };
+export { chop };
+export { chopRight };
+export { clean };
+export { count };
+export { decapitalize };
+export { escape };
+export { extract };
+export { insert };
+export { isBlank };
+export { isChar };
+export { isString };
+export { join };
+export { lines };
+export { pad };
+export { padLeft };
+export { padRight };
+export { remove };
+export { repeat };
+export { reverse };
+export { swap };
+export { template };
+export { trim };
+export { trimLeft };
+export { trimRight };
+export { truncate };
+export { unescape };
+
+export default {
+    camelize,
+    capitalize,
+    chop,
+    chopRight,
+    clean,
+    count,
+    decapitalize,
+    escape,
+    extract,
+    insert,
+    isBlank,
+    isChar,
+    isString,
+    join,
+    lines,
+    pad,
+    padLeft,
+    padRight,
+    remove,
+    repeat,
+    reverse,
+    swap,
+    template,
+    trim,
+    trimLeft,
+    trimRight,
+    truncate,
+    unescape
+};
