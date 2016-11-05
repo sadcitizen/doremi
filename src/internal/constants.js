223f249aead60fbd04e898854042b16793441b2b
sadcitizen <sadcitizen@yandex.ru>
2016-11-06 01:57:38 +0500

    constants fixes

--- src/internal/constants.js
+++ src/internal/constants.js
@@ -1,8 +1,11 @@
-var MAX_INT = 2147483647; //Maximum 32-bit signed integer value. (2^31 - 1)
+export const MAX_INT = 2147483647; //Maximum 32-bit signed integer value. (2^31 - 1)
+export const MIN_INT = -MAX_INT;
+export const DIGITS = '0123456789';
+export const HEX_LETTERS = 'abcdef';
 
 export default {
-    MAX_INT: MAX_INT,
-    MIN_INT: -MAX_INT,
-    DIGITS: '0123456789',
-    HEX_LETTERS: 'abcdef'
+    MAX_INT,
+    MIN_INT,
+    DIGITS,
+    HEX_LETTERS
 };
