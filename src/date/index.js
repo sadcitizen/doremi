6b4a19380c5a8cc19f4419b70ba9a5e292f4c44a
abrosimov <abrosimov@byndyusoft.com>
2016-11-27 02:08:09 +0500

    index for "date" functions

--- src/date/index.js
+++ src/date/index.js
@@ -1,9 +1,25 @@
+import add from './add';
+import clearTime from './clear-time';
 import clone from './clone';
-import format from './format';
 import diff from './diff';
+import format from './format';
+import isDate from './is-date';
+import isValid from './is-valid';
+
+export { add };
+export { clearTime };
+export { clone };
+export { diff };
+export { format };
+export { isDate };
+export { isValid };
 
 export default {
+    add,
+    clearTime,
     clone,
+    diff,
     format,
-    diff
+    isDate,
+    isValid
 };
