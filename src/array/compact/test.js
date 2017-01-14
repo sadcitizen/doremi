8fa14cbf7bc23b197fe44a98b1b92591c31dc383
sadcitizen <sadcitizen@yandex.ru>
2017-01-15 03:01:30 +0500

    fix test descriptions

--- src/array/compact/test.js
+++ src/array/compact/test.js
@@ -2,12 +2,12 @@ import { expect } from 'chai';
 import compact from '.';
 
 describe('array/compact()', () => {
-    it('Return array without null or undefined', () => {
+    it('Returns the array without null or undefined values', () => {
         expect(compact([0, 1, false, 2, null, 3, undefined, '', 'null'])).to.deep.equal([0, 1, false, 2, 3, '', 'null']);
         expect(compact([null, undefined])).to.deep.equal([]);
     });
 
-    it('Return empty array if source is empty array', () => {
+    it('Returns the empty array if source is empty array', () => {
         expect(compact([])).to.deep.equal([]);
     });
 });
--- src/array/distinct/test.js
+++ src/array/distinct/test.js
@@ -3,14 +3,14 @@ import { expect } from 'chai';
 import distinct from '.';
 
 describe('array/distinct()', () => {
-    it('Return distinct elements from an array', () => {
+    it('Returns distinct elements from the array', () => {
         const target = [1, 2, 1, 3, 1, 4];
         const expected = [1, 2, 3, 4];
 
         expect(distinct(target)).to.deep.equal(expected);
     });
 
-    it('Allow getter to determine equality', () => {
+    it('Allows getter to determine equality', () => {
         const target = [{name: 'Casper'}, {name: 'Richard'}, {name: 'Casper'}];
         const expected = [{name: 'Casper'}, {name: 'Richard'}];
         const getter = i => i.name;
