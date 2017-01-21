81226e721626039be7bb8d9c2f5054a2e3f53172
sadcitizen <sadcitizen@yandex.ru>
2017-01-22 02:57:07 +0500

    add description

--- src/array/union/index.js
+++ src/array/union/index.js
@@ -1,6 +1,19 @@
 import distinct from '../distinct';
 import identity from '../../func/identity';
 
+/**
+ * Concats the arrays and removes duplicates.
+ * @param {Array} target The first array to concat.
+ * @param {Array} [source=[]] The second array to concat.
+ * @param {Function} [iteratee=identity] The function invoked per iteration.
+ * @return {Array} Returns the concated array.
+ *
+ * @example
+ * const a = ['a', 'b', 'c'];
+ * const b = ['d', 'e', 'f'];
+ * union(a, b);
+ * // => ['a', 'b', 'c', 'd', 'e', 'f']
+ */
 export default function (target, source = [], iteratee = identity) {
     return distinct(target.concat(source), iteratee);
 }
--- src/array/update/index.js
+++ src/array/update/index.js
@@ -7,6 +7,11 @@ import identity from '../../func/identity';
  * @param {number} index The index of the element.
  * @param {Function} [modifier] The function to apply.
  * @return {Array} Returns new cope of the array.
+ *
+ * @example
+ * const target = [1, 2, 3, 4, 5];
+ * update(target, 1, () => 3);
+ * // => [1, 3, 3, 4, 5]
  */
 export default function (target, index, modifier = identity) {
     if (index < 0 || index >= target.length) {
