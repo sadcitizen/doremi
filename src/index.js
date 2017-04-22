a86fd56eb79ccf0f28fda2b653f9415bcffdc0f5
sadcitizen <sadcitizen@yandex.ru>
2017-04-22 19:42:06 +0500

    add index.js and import tests for "doremi"

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import array from './array';
+import common from './common';
 import date from './date';
 import func from './func';
+import logic from './logic';
 import number from './number';
+import object from './object';
 import random from './random';
+import string from './string';
+
+export { array };
+export { common };
+export { date };
+export { func };
+export { logic };
+export { number };
+export { object };
+export { random };
+export { string };
 
 export default {
     array,
+    common,
     date,
     func,
+    logic,
     number,
-    random
+    object,
+    random,
+    string
 };
