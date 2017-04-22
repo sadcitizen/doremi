02960055535c292a2b16d69b3ddc052e65d515f4
sadcitizen <sadcitizen@yandex.ru>
2017-04-22 19:39:20 +0500

    add index.js and import tests for "array"

--- src/array/index.js
+++ src/array/index.js
@@ -1,8 +1,12 @@
 import append from './append';
 import at from './at';
 import compact from './compact';
+import countBy from './count-by';
+import distinct from './distinct';
+import equals from './equals';
 import first from './first';
 import flatten from './flatten';
+import groupBy from './group-by';
 import last from './last';
 import max from './max';
 import min from './min';
@@ -11,14 +15,39 @@ import prepend from './prepend';
 import remove from './remove';
 import sortBy from './sort-by';
 import sum from './sum';
+import union from './union';
 import update from './update';
+
+export { append };
+export { at };
+export { compact };
+export { countBy };
+export { distinct };
+export { equals };
+export { first };
+export { flatten };
+export { groupBy };
+export { last };
+export { max };
+export { min };
+export { partition };
+export { prepend };
+export { remove };
+export { sortBy };
+export { sum };
+export { union };
+export { update };
 
 export default {
     append,
     at,
     compact,
+    countBy,
+    distinct,
+    equals,
     first,
     flatten,
+    groupBy,
     last,
     max,
     min,
@@ -27,5 +56,6 @@ export default {
     remove,
     sortBy,
     sum,
+    union,
     update
 };
