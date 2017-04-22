4d3dbde49ef14ef9a112ee202a7eeb6c7027c194
sadcitizen <sadcitizen@yandex.ru>
2017-04-22 19:39:38 +0500

    add index.js and import tests for "random"

--- src/random/index.js
+++ src/random/index.js
@@ -5,6 +5,13 @@ import guid from './guid';
 import hex from './hex';
 import int from './int';
 
+export { bool };
+export { choice };
+export { float };
+export { guid };
+export { hex };
+export { int };
+
 export default {
     bool,
     choice,
