944705815d835ee308fde331c0c4b57fd5a33d95
sadcitizen <sadcitizen@yandex.ru>
2018-02-23 20:22:47 +0500

    fixes names of test cases

--- src/date/start-of/tests.js
+++ src/date/start-of/tests.js
@@ -7,32 +7,32 @@ describe('date/startOf()', () => {
         date = new Date(2015, 2, 23, 23, 20, 11, 491);
     });
 
-    test('Returns start of the year', () => {
+    test('returns start of the year', () => {
         expect(startOf(date, 'year')).toEqual(new Date(2015, 0, 1));
         expect(startOf(date, 'Y')).toEqual(new Date(2015, 0, 1));
     });
 
-    test('Returns start of the month', () => {
+    test('returns start of the month', () => {
         expect(startOf(date, 'month')).toEqual(new Date(2015, 2, 1));
         expect(startOf(date, 'M')).toEqual(new Date(2015, 2, 1));
     });
 
-    test('Returns start of the day', () => {
+    test('returns start of the day', () => {
         expect(startOf(date, 'day')).toEqual(new Date(2015, 2, 23));
         expect(startOf(date, 'D')).toEqual(new Date(2015, 2, 23));
     });
 
-    test('Returns start of the hour', () => {
+    test('returns start of the hour', () => {
         expect(startOf(date, 'hour')).toEqual(new Date(2015, 2, 23, 23));
         expect(startOf(date, 'h')).toEqual(new Date(2015, 2, 23, 23));
     });
 
-    test('Returns start of the minute', () => {
+    test('returns start of the minute', () => {
         expect(startOf(date, 'minute')).toEqual(new Date(2015, 2, 23, 23, 20));
         expect(startOf(date, 'm')).toEqual(new Date(2015, 2, 23, 23, 20));
     });
 
-    test('Returns start of the second', () => {
+    test('returns start of the second', () => {
         expect(startOf(date, 'second')).toEqual(new Date(2015, 2, 23, 23, 20, 11));
         expect(startOf(date, 's')).toEqual(new Date(2015, 2, 23, 23, 20, 11));
     });
