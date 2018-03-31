e9560236ef5ca07a6dfbbf3e4bb814e312cd189b
sadcitizen <sadcitizen@yandex.ru>
2018-03-31 22:14:50 +0500

    updates number/equals docs

--- src/number/equals/index.js
+++ src/number/equals/index.js
@@ -1,8 +1,8 @@
 /**
- * @param {number} value The number to compare.
- * @param {number} other The number to compare.
- * @param {number} accuracy The maximum difference of the numbers at which they are equal.
- * @returns {boolean} Returns true if the numbers are equal, false otherwise.
+ * @param {number} value
+ * @param {number} other
+ * @param {number} accuracy
+ * @returns {boolean}
  */
 export default function (value, other, accuracy = 0.001) {
     return Math.abs(value - other) < accuracy;
--- src/number/equals/readme.md
+++ src/number/equals/readme.md
@@ -1,10 +1,15 @@
-### equals
+# equals(value, other, accuracy)
 
 Checks if two numbers are approximately equal.
 
-#### Example
+## Usage
 
 ```js
-equals(Math.E, 2.7, 0.1); // => true
-equals(Math.PI, 3.1415, 0.0001); // => true
+import equals from 'doremi/number/equals';
+
+equals(Math.E, 2.7, 0.1);
+// => true
+
+equals(Math.PI, 3.1415, 0.0001);
+// => true
 ```
