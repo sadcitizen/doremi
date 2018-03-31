6e92766b46af569582f0f936476349f41f306817
sadcitizen <sadcitizen@yandex.ru>
2018-03-31 10:46:29 +0500

    updates date/equals docs

--- src/date/equals/index.js
+++ src/date/equals/index.js
@@ -1,10 +1,10 @@
 import startOf from '../start-of';
 
 /**
- * @param {Date} value The date to compare.
- * @param {Date} other The date to compare.
- * @param {('year'|'Y'|'month'|'M'|'day'|'D'|'hour'|'h'|'minute'|'m'|'second'|'s'|'millisecond'|'ms')} unit The time unit.
- * @returns {boolean} True if dates are equal, false otherwise.
+ * @param {Date} value
+ * @param {Date} other
+ * @param {('year'|'Y'|'month'|'M'|'day'|'D'|'hour'|'h'|'minute'|'m'|'second'|'s'|'millisecond'|'ms')} unit
+ * @returns {boolean}
  */
 export default function (value, other, unit = 'ms') {
     if (unit === 'ms' || unit === 'millisecond') {
--- src/date/equals/readme.md
+++ src/date/equals/readme.md
@@ -1,18 +1,18 @@
-### equals
+# equals(value, other, unit)
 
-Checks if the dates are equal. 
+Checks if the dates are equal with time unit.
 
-#### Example
-
-```js
-const value = new Date(2015, 2, 23);
-const other = new Date(2015, 2, 23);
-equals(value, other); // => true
-```
+## Usage
 
 ```js
+import equals from 'doremi/date/equals';
+
+equals(new Date(2015, 2, 23), new Date(2015, 2, 23));
+// => true
+
 const value = new Date(2015, 2, 23, 3, 10);
 const other = new Date(2015, 2, 23, 3, 13);
+
 equals(value, other, 'minute'); // => false
 ```
 
