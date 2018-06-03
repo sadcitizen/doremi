c20e6dc840959dc9c087e5b23a7c062c663fec20
sadcitizen <hi@sadcitizen.me>
2018-06-03 16:20:42 +0500

    undefined as the constant

--- src/internal/constants.js
+++ src/internal/constants.js
@@ -2,10 +2,12 @@ export const MAX_INT = 2147483647; // Maximum 32-bit signed integer value. (2^31
 export const MIN_INT = -MAX_INT;
 export const DIGITS = '0123456789';
 export const HEX_LETTERS = 'abcdef';
+export const UNDEF = undefined; // eslint-disable-line no-undefined
 
 export default {
     MAX_INT,
     MIN_INT,
     DIGITS,
-    HEX_LETTERS
+    HEX_LETTERS,
+    UNDEF
 };
