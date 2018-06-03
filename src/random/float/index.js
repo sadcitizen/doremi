0f58b34a57d7686290e94abea2de496dfac2adeb
Eugene Abrosimov <hi@sadcitizen.me>
2018-06-03 23:10:06 +0500

    updates random/float()

--- src/random/float/index.js
+++ src/random/float/index.js
@@ -12,12 +12,12 @@ const { floor, random } = Math;
 export default function (min, max, precision = 5) {
     const frac = random();
 
-    min = isDefined(min) ? ~~min : MIN_INT;
-    max = isDefined(max) ? ~~max : MAX_INT;
+    const minValue = isDefined(min) ? min : MIN_INT;
+    const maxValue = isDefined(max) ? max : MAX_INT;
 
-    if (min > max) {
+    if (minValue > maxValue) {
         throw new Error('Minimum value cannot be greater than maximum value.');
     }
 
-    return parseFloat((floor(random() * (max - min - frac)) + frac).toFixed(precision));
+    return parseFloat((floor(random() * (maxValue - minValue - frac)) + frac).toFixed(precision));
 }
