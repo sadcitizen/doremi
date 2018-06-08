0ba3de2a2549af6e3d634eba86d0a797f876d408
Eugene Abrosimov <hi@sadcitizen.me>
2018-06-08 22:30:51 +0500

    array/group => array/groupBy

--- src/array/group-by/tests.js
+++ src/array/group-by/tests.js
@@ -1,20 +1,20 @@
-import group from '.';
+import groupBy from '.';
 
-describe('array/group()', () => {
+describe('array/groupByBy()', () => {
     test('returns grouped elements of the array by value', () => {
         const value = [1, 2, 3, 3, 2, 4];
 
-        expect(group(value)).toEqual({ 1: [1], 2: [2, 2], 3: [3, 3], 4: [4] });
+        expect(groupBy(value)).toEqual({ 1: [1], 2: [2, 2], 3: [3, 3], 4: [4] });
     });
 
     test('returns empty object for empty array', () => {
-        expect(group([])).toEqual({});
+        expect(groupBy([])).toEqual({});
     });
 
     test('allows custom iterator', () => {
         const cities = [{ name: 'Moscow' }, { name: 'London' }, { name: 'Tokyo' }, { name: 'Paris' }, { name: 'Berlin' }];
 
-        expect(group(cities, city => city.name.length)).toEqual({
+        expect(groupBy(cities, city => city.name.length)).toEqual({
             5: [{ name: 'Tokyo' }, { name: 'Paris' }],
             6: [{ name: 'Moscow' }, { name: 'London' }, { name: 'Berlin' }]
         });
