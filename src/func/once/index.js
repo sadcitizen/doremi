04c17a396c74ddb0e9c3aee130a513f23dd5ce6b
Eugene Abrosimov <hi@sadcitizen.me>
2018-06-14 23:20:41 +0500

    func/once

--- src/func/once/index.js
+++ src/func/once/index.js
@@ -2,18 +2,6 @@ import before from '../before';
 import partialRight from '../partial-right';
 
 /**
- * Returns a new function that invokes once and store the result of last call.
- *
  * @return Returns the new function.
- *
- * @example
- * const add = (x, y) => x +y;
- * const addOnce = once(add);
- * addOnce(1, 2);
- * // => 3
- * addOnce(3, 4);
- * // => 3
- * addOnce(5, 6);
- * // => 3
  */
 export default partialRight(before, 2);
