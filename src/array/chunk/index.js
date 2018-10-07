7a28219df201e92b788c95e0d05d70840a41c000
sadcitizen <hi@sadcitizen.me>
2018-10-07 15:35:48 +0500

    review

--- src/array/chunk/index.js
+++ src/array/chunk/index.js
@@ -6,7 +6,7 @@ import { INVALID_ARRAY_FIRST_ARGUMENT } from '../../constants/errors';
  * @category array
  * @param {Array} value The array to split.
  * @param {number} size The length of each chunk.
- * @returns {Array}
+ * @returns {Array} Returns new array of chunks.
  */
 export default function (value, size = 0) {
     if (!Array.isArray(value)) {
