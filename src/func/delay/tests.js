e2133c7339b1ff63886893324b7336784e6b1003
sadcitizen <hi@sadcitizen.me>
2018-11-06 10:10:04 +0500

    add func/delay cancellation test case

--- src/func/delay/tests.js
+++ src/func/delay/tests.js
@@ -22,6 +22,25 @@ describe('func/delay()', () => {
         jest.clearAllTimers();
     });
 
+    test('cancels function before invocation', () => {
+        jest.useFakeTimers();
+
+        const fn = jest.fn();
+        const delayed = delay(fn, 50);
+
+        const result = delayed();
+
+        jest.runTimersToTime(45);
+        expect(fn).not.toHaveBeenCalled();
+
+        result.cancel();
+
+        jest.runTimersToTime(100);
+        expect(fn).not.toHaveBeenCalled();
+
+        jest.clearAllTimers();
+    });
+
     test('throws exception if function is invalid', () => {
         expect(() => delay('jest.fn()', 100)).toThrow(INVALID_FUNCTION_ARGUMENT);
     });
